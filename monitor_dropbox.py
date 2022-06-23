2e834481dcec8f459c62e112dc81f903444efae7
Jessi Jassal <jessica_jassal@harvard.edu>
2022-06-23 14:35:53 -0400

    Catch general exception

--- monitor_dropbox.py
+++ monitor_dropbox.py
@@ -42,7 +42,7 @@ def notify_dts_loadreports(filename):
         response = get(dts_endpoint + '/loadreport?filename=' + filename, verify=False)
         logging.debug("Response status code for '/loadreport?filename='" + filename + ": " + response.status_code)
         response.raise_for_status()
-    except (exceptions.ConnectionError, HTTPError) as e:
+    except Exception as e:
         logging.error("Error when calling DTS /loadreport for file: " + str(e))
 
 
@@ -66,7 +66,7 @@ def notify_dts_failed_batch(batch_name):
         response = get(dts_endpoint + '/failedBatch?batchName=' + batch_name, verify=False)
         logging.debug("Response status code for '/failedBatch?batchName='" + batch_name + ": " + response.status_code)
         response.raise_for_status()
-    except (exceptions.ConnectionError, HTTPError) as e:
+    except Exception as e:
         logging.error("Error when calling DTS /loadreport for file: " + str(e))
 
 
